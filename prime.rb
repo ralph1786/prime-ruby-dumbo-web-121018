d48e24c26eaa490586902f72dfff1bdfe1d17b03
Rafael Cruz <ralph1786@gmail.com>
2018-11-14 21:05:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,7 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  elsif Math.sqrt(number).to_i.downto(2).each {|i| return false if number % i == 0}
+  return true
+end
+end
